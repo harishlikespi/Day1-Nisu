5afb4aedda76103ba80b8d894adcd7c35fdffe54
harishlikespi <73505107+harishlikespi@users.noreply.github.com>
2020-11-16 16:25:29 -0800

    Update Test.c

--- Test.c
+++ Test.c
@@ -15,3 +15,5 @@ void printStars() {
     }
     return 0;
 }
+// Your void method has a return for an int, and while it still runs the compiler will send out a warning. Also, when calling methods, you need a protoype of the method if the method is written
+// after wherever you're calling it, and again, while it still runs, the compiler will give you an implicit decleration warning.
